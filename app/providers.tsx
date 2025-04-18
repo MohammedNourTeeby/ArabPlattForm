47cb8a5585dfc381b6469ec78598e9ba6c2d7730
mohammednourteeby <mohammednourteeby@gmail.com>
2025-04-18 21:26:44 +0300

    add Reports

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,22 +1,16 @@
 // app/providers.tsx
 'use client';
 import { ReactNode } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { MantineProvider } from '@mantine/core';
 import { MediaRecorderProvider } from '@/contexts/MediaRecorderContext';
 
-const queryClient = new QueryClient({
-  defaultOptions: { queries: { refetchOnWindowFocus: false, retry: 1 } },
-});
+
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
       <MantineProvider  >
         <MediaRecorderProvider>{children}</MediaRecorderProvider>
       </MantineProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
   );
 }
